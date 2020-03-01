27f5f34c386028945aac261ffcd04f19ef6c5ba2
github username <josephtrettevik@gmail.com>
2020-02-29 18:23:49 -0800

    moving all styling into a StyleSheet variable

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import { Image, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={{width: 305, height: 159}} />
-      <Text style={{color: '#888', fontSize: 18}}>
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+      <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button below!
       </Text>
     </View>
@@ -20,4 +20,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  }, 
 });
