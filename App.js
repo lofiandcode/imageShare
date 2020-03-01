79a6d0ec52a9cc59c03a7e7a50bc1b035210aa83
github username <josephtrettevik@gmail.com>
2020-02-29 18:19:12 -0800

    added a photo about the text

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={{width: 305, height: 159}} />
       <Text style={{color: '#888', fontSize: 18}}>
         To share a photo from your phone with a friend, just press the button below!
       </Text>
