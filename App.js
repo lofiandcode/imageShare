74169c7fa0307c13cd39ea1f27ef73341cc99cfb
github username <josephtrettevik@gmail.com>
2020-03-03 13:19:49 -0800

    added the ability to share a photo

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-
+import * as Sharing from 'expo-sharing';
 
 export default function App() {
   let [selectedImage, setSelectedImage] = React.useState(null);
@@ -25,6 +25,15 @@ export default function App() {
     setSelectedImage({ localUri: pickerResult.uri });
   };
 
+  const openShareDialogAsync = async () => {
+    if(!(await Sharing.isAvailableAsync())) {
+      alert("Uh oh, sharing isn't available on your platform");
+      return;
+    }
+
+    Sharing.shareAsync(selectedImage.localUri);
+  };
+
   if (selectedImage !== null) {
     return (
       <View style={styles.container}>
@@ -32,6 +41,9 @@ export default function App() {
           source={{ uri: selectedImage.localUri }}
           style={styles.thumbnail}
         />
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Share this photo</Text>
+        </TouchableOpacity>
       </View>
     );
   }
